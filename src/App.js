f072b39f35d8a1c7b78dd943c75bd4d26028752d
MrKamake <seongjooon@gmail.com>
2019-12-12 17:17:36 +0900

    add function of update for

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
 module.exports = function update(prevState, changes) {
-  // type here
+  const result = getChangesState(changes);
+
+  function setNextState(keys, changesValue) {
+    const obj = {};
+    const temp = keys.pop();
+
+    if (!temp) {
+      return { ...prevState, ...changesValue };
+    }
+
+    obj[temp] = changesValue;
+    return setNextState(keys, obj);
+  }
+
+  function getChangesState(changesState, ...keys) {
+    for (const prop in changesState) {
+      if (typeof changesState[prop] === 'object') {
+        if (prop === '$set') {
+          return changesState[prop];
+        }
+        return getChangesState(changesState[prop], ...keys.concat(prop));
+      }
+      return setNextState(keys, changesState[prop]);
+    }
+  }
+
+  return result;
 };
